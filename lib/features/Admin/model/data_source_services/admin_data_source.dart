b45b01c7b0dcb0c52bb3ded023ee4abed87d9a79
Menna Walid <mennafadel12@gmail.com>
2025-06-30 00:02:11 +0300

    removed admin folder to admin app

--- lib/features/Admin/model/data_source_services/admin_data_source.dart
+++
@@ -1,5 +0,0 @@
-import 'package:sign2/features/Admin/model/data_source_services/admin_firebase_impl.dart';
-
-abstract class AdminDataSource {
-  Future<SignInResult> signIn(String adminId, String password);
-}
