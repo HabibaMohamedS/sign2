3d956d7d4baaca5bab6ec7f59da66c94760600ae
mariam2002212 <mariam2002azima@gmail.com>
2025-07-02 03:01:46 +0300

    +retake quiz btn +framed custom btn

--- lib/features/Quizzes/view/grade_screen.dart
+++ lib/features/Quizzes/view/grade_screen.dart
@@ -3,118 +3,116 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:sign2/features/Learning_feature/view/learn_sl_screen.dart';
 import 'package:sign2/features/Quizzes/view_model/grade_controller.dart';
-
+import 'package:sign2/features/Quizzes/view_model/quiz_controller.dart';
+import 'package:sign2/support/custom_widgets/framed_custom_btn.dart';
 import '../../../support/custom_widgets/custom_elevated_button.dart';
 import '../../../support/theme/app_colors.dart';
 import '../../../support/theme/app_text_styles.dart';
 
 class GradeScreen extends StatelessWidget {
   static const String routeName = "/gradeScreen";
+
   const GradeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final args = Get.arguments as Map<String, int>;
+    final args = Get.arguments as Map<String, dynamic>;
+    final correct = args['correct'] ?? 0;
+    final incorrect = args['incorrect'] ?? 0;
+    final playlistId = args['playlistId'] ?? '';
+    final title = args['title'] ?? '';
+
     final GradeController controller = Get.put(
-      GradeController(
-        correctAnswers: args['correct'] ?? 0,
-        incorrectAnswers: args['incorrect'] ?? 0,
-      ), // will get them from the quiz controller
+      GradeController(correctAnswers: correct, incorrectAnswers: incorrect),
     );
 
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color(0xFFF9F3FF),
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 30.h),
-      
-                Center(
-                  child: Text(
-                    'Good Job!',
-                    style: AppTextStyle.heading.copyWith(
-                      color: AppColors.darkPurple,
-                      fontSize: 18.sp
-                    ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 30.h),
+              Center(
+                child: Text(
+                  'Good Job!',
+                  style: AppTextStyle.heading.copyWith(
+                    color: AppColors.darkPurple,
+                    fontSize: 18.sp,
                   ),
                 ),
-      
-                SizedBox(height: 20.h),
-      
-                Container(
-                  width: 400.h,
-                  height: 350.h,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16.r),
-                    border: Border.all(color: Colors.transparent),
-                  ),
-                  child: Stack(
-                    children: [
-                         Image.asset(
-                          'assets/images/Trophy.png',
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                          height: double.infinity,
-                        ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          width: double.infinity,
-                          padding: EdgeInsets.symmetric(vertical: 12.h),
-                          child: Text(
-                            'You got +${controller.totalPoints} quiz points!',
-                            textAlign: TextAlign.center,
-                            style: AppTextStyle.QHeader,
-                          ),
+              ),
+              SizedBox(height: 20.h),
+              Container(
+                width: 400.h,
+                height: 350.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.r),
+                ),
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      'assets/images/Trophy.png',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 12.h),
+                        child: Text(
+                          'You got +${controller.totalPoints} quiz points!',
+                          textAlign: TextAlign.center,
+                          style: AppTextStyle.QHeader,
                         ),
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30.h),
+              Text('Correct answers',
+                  style: AppTextStyle.QBody.copyWith(color: AppColors.darkPurple)),
+              Text('$correct questions',
+                  style: AppTextStyle.QBody.copyWith(fontWeight: FontWeight.bold)),
+              SizedBox(height: 20.h),
+              Text('Incorrect answers',
+                  style: AppTextStyle.QBody.copyWith(color: AppColors.darkPurple)),
+              Text('$incorrect questions',
+                  style: AppTextStyle.QBody.copyWith(fontWeight: FontWeight.bold)),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FramedCustomButton(
+                    buttonText: 'Retake',
+                    onPressed: () async {
+                      final quizController = Get.find<QuizController>();
+                      quizController.resetQuiz();
+                      await quizController.generateQuizFromPlaylist(
+                        playlistId: playlistId,
+                        title: title,
+                      );
+                    },
                   ),
-                ),
-      
-                SizedBox(height: 30.h),
-      
-                Text(
-                  'Correct answers',
-                  style: AppTextStyle.QBody.copyWith(color: AppColors.darkPurple),
-                ),
-                Text(
-                  '${controller.correctAnswers} questions',
-                  style: AppTextStyle.QBody.copyWith(fontWeight: FontWeight.bold),
-                ),
-      
-                SizedBox(height: 20.h),
-      
-                Text(
-                  'Incorrect answers',
-                  style: AppTextStyle.QBody.copyWith(color: AppColors.darkPurple),
-                ),
-                Text(
-                  '${controller.incorrectAnswers} questions',
-                  style: AppTextStyle.QBody.copyWith(fontWeight: FontWeight.bold),
-                ),
-      
-                const Spacer(),
-      
-                CustomElevatedButton(
-                  buttonText: 'Next',
-                  onPressed: () {
-                    Get.offAllNamed(
-                      LearnSlScreen.routeName,
-                    );
-                  },
-                ),
-      
-                SizedBox(height: 20.h),
-              ],
-            ),
+                  CustomElevatedButton(
+                    buttonText: 'Done',
+                    onPressed: () {
+                      Get.offAllNamed(LearnSlScreen.routeName);
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.h),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+
--- lib/features/Quizzes/widgets/option_tile.dart
+++ lib/features/Quizzes/widgets/option_tile.dart
@@ -1,53 +1,53 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:sign2/features/Quizzes/view_model/quiz_controller.dart';
+ import 'package:flutter/material.dart';
+ import 'package:get/get.dart';
+ import 'package:sign2/features/Quizzes/view_model/quiz_controller.dart';
 
-class OptionTile extends StatelessWidget {
+ class OptionTile extends StatelessWidget {
   final String label;
   final int index;
   final bool isSelected;
   final bool isSubmitted;
   final bool isCorrect;
+  final QuizController controller;
 
   const OptionTile({
-    required this.label,
-    required this.index,
-    required this.isSelected,
-    required this.isSubmitted,
-    required this.isCorrect,
-    super.key,
+   required this.label,
+   required this.index,
+   required this.isSelected,
+   required this.isSubmitted,
+   required this.isCorrect,
+   required this.controller,
+   super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    IconData? feedbackIcon;
-    Color? iconColor;
+   IconData? feedbackIcon;
+   Color? iconColor;
 
-    if (isSubmitted) {
-      if (isSelected && isCorrect) {
-        feedbackIcon = Icons.check;
-        iconColor = Colors.green;
-      } else if (isSelected && !isCorrect) {
-        feedbackIcon = Icons.close;
-        iconColor = Colors.red;
-      } else if (!isSelected && isCorrect) {
-        feedbackIcon = Icons.check;
-        iconColor = Colors.green;
-      }
+   if (isSubmitted) {
+    if (isSelected && isCorrect) {
+     feedbackIcon = Icons.check;
+     iconColor = Colors.green;
+    } else if (isSelected && !isCorrect) {
+     feedbackIcon = Icons.close;
+     iconColor = Colors.red;
+    } else if (!isSelected && isCorrect) {
+     feedbackIcon = Icons.check;
+     iconColor = Colors.green;
     }
+   }
 
-    return ListTile(
-      leading: Radio<int>(
-        value: index,
-        groupValue: Get.find<QuizController>().selectedOption.value,
-        onChanged: isSubmitted ? null : (value) {
-          Get.find<QuizController>().selectOption(value!);
-        },
-      ),
-      title: Text(label),
-      trailing: feedbackIcon != null
-          ? Icon(feedbackIcon, color: iconColor)
-          : null,
-    );
+   return RadioListTile<int>(
+    value: index,
+    groupValue: controller.selectedOption.value,
+    onChanged: isSubmitted ? null : (value) {
+     if (value != null) controller.selectOption(value);
+    },
+    title: Text(label),
+    secondary: feedbackIcon != null
+        ? Icon(feedbackIcon, color: iconColor)
+        : null,
+   );
   }
-}
+ }
