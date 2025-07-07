6bae94ea02bd70b0cc4512cecf1b856e3880395a
Menna Walid <mennafadel12@gmail.com>
2025-07-08 02:12:47 +0300

    removed avatar

--- lib/features/profile/viewmodel/profile_controller.dart
+++ lib/features/profile/viewmodel/profile_controller.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart' as fstore;
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:sign2/app/routes/app_routes.dart';
 import 'package:sign2/features/Auth/Controller/firebase_auth.dart';
 import 'package:sign2/features/Auth/model/UserModel.dart';
+import 'package:sign2/features/menu/main_menu_screen.dart';
 import 'package:sign2/services/storage_services.dart';
 
 class ProfileController extends GetxController {
@@ -27,34 +26,6 @@ class ProfileController extends GetxController {
   late RxString government  = _user.government.obs;
 
   String get email     => _user.email;
-  String? get imageUrl => _user.imageUrl;
-
-  //Avatar
-  Future<void> changeAvatar(ImageSource source) async {
-    final picked = await picker.pickImage(source: source, imageQuality: 85);
-    if (picked == null) return;
-
-    avatarFile.value = File(picked.path);
-
-    final uid = FirebaseAuth.instance.currentUser?.uid;
-    if (uid == null) {
-      Get.snackbar('Error', 'User not logged in.');
-      return;
-    }
-
-    final ref = fstore.FirebaseStorage.instance
-        .ref()
-        .child('avatars/$uid.jpg');
-
-    await ref.putFile(avatarFile.value!);
-    final url = await ref.getDownloadURL();
-
-    await _firestore.collection('users').doc(uid).update({'imageUrl': url});
-
-    //cache
-    _cache.cachedUser = _cache.cachedUser!.copyWith(imageUrl: url);
-    Get.snackbar('Profile', 'Photo updated successfully');
-  }
 
   //Save
   Future<void> saveChanges() async {
@@ -84,6 +55,6 @@ class ProfileController extends GetxController {
   //Logout 
   Future<void> logout() async {
     await _authService.logout();
-    Get.offAllNamed(AppRoutes.login);
+    Get.offAllNamed(MainMenuScreen.routeName);
   }
 }
